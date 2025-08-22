afa7fb351a4a4012d9b1cad6c18941b371083f9b
moretolearn <yanumularammohan661@gmail.com>
2025-08-22 10:28:32 +0530

    Added Token implementation

--- src/main/java/com/moretolearn/client/JsonPlaceholderClient.java
+++ src/main/java/com/moretolearn/client/JsonPlaceholderClient.java
@@ -2,6 +2,7 @@ package com.moretolearn.client;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.service.annotation.*;
 
 import com.moretolearn.model.Post;
@@ -25,4 +26,7 @@ public interface JsonPlaceholderClient {
 
     @DeleteExchange("/{id}")
     void deletePost(@PathVariable int id);
+    
+    @GetExchange("/{id}")
+    Post getPostByIdWithToken(@PathVariable int id, @RequestHeader("Authorization") String token);
 }
--- src/main/java/com/moretolearn/config/RestClientConfig.java
+++ src/main/java/com/moretolearn/config/RestClientConfig.java
@@ -10,16 +10,36 @@ import com.moretolearn.client.JsonPlaceholderClient;
 
 @Configuration
 public class RestClientConfig {
-	
+
 	@Bean
 	JsonPlaceholderClient jsonPlaceholderClient() {
-		return createClient(JsonPlaceholderClient.class, "https://jsonplaceholder.typicode.com");
+		return createClientWithOutTOken(JsonPlaceholderClient.class, "https://jsonplaceholder.typicode.com");
 	}
-	
-	public <T> T createClient(Class<T> clientType, String baseUrl) {
-        RestClient restClient = RestClient.builder().baseUrl(baseUrl).build();
-        HttpServiceProxyFactory factory =
-                HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
-        return factory.createClient(clientType);
-    }
+
+	public <T> T createClientWithOutTOken(Class<T> clientType, String baseUrl) {
+		RestClient restClient = RestClient.builder().baseUrl(baseUrl).build();
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient))
+				.build();
+		return factory.createClient(clientType);
+	}
+
+	public <T> T createClientWithStaticToken(Class<T> clientType, String baseUrl) {
+		RestClient restClient = RestClient.builder().baseUrl(baseUrl)
+				.defaultHeader("Authorization", "Bearer YOUR_TOKEN_HERE").build();
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient))
+				.build();
+		return factory.createClient(clientType);
+	}
+
+	public <T> T createClientWithDynamicToken(Class<T> clientType, String baseUrl) {
+		RestClient restClient = RestClient.builder().baseUrl(baseUrl).requestInterceptor((request, body, execution) -> {
+			String token = "Bearer " + "token from server";
+			request.getHeaders().set("Authorization", token);
+			return execution.execute(request, body);
+		}).build();
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient))
+				.build();
+		return factory.createClient(clientType);
+	}
+
 }
